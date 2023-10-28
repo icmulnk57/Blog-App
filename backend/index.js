04a87ae5338c56e0444054a814b1d2912fcde4ca
Aman <mandalamankumar.786@gmail.com>
2023-10-28 10:32:44 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -21,8 +21,8 @@ require('dotenv').config();
 app.use(express.json())
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({origin:"https://main--lambent-conkies-0b36c0.netlify.app",credentials:true}))
-app.use(cors({origin:"https://lambent-conkies-0b36c0.netlify.app",credentials:true}))
+app.use(cors({origin:"https://dailyhunts.netlify.app",credentials:true}))
+
 
 
 // if(process.env.Node_ENV="production")
