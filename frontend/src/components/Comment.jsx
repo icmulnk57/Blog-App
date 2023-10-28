e0ec9e57089c5ce7f6a0f425d40da767ea020515
Aman <mandalamankumar.786@gmail.com>
2023-10-28 13:41:19 +0530

    updated

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -7,15 +7,18 @@ import { UserContext } from '../context/UserContext'
 
 const Comment = ({c,post}) => {
   const {user}=useContext(UserContext);
+  const [isDeleting, setIsDeleting] = useState(false);
   const deleteComment=async(id)=>{
     try{
-
+      setIsDeleting(true);
       await axios.delete(URL+`/api/comments/`+id,{withCredentials:true});
-      window.location.reload(true)
+   
       
       
     }catch(err){
       console.log(err);
+    }finally {
+      setIsDeleting(false);
     }
   }
   
@@ -25,24 +28,24 @@ const Comment = ({c,post}) => {
     <div className="flex items-center justify-between">
       <h3 className="font-bold text-gray-600">@{c.author}</h3>
       <div className="flex justify-center items-center space-x-4">
-      <p>{new Date(c.updatedAt).toString().slice(0, 15)}</p>
-            <p>{new Date(c.updatedAt).toString().slice(16, 24)}</p>
-
-            {
-              user?._id===c?.userId?
-              <div className="flex items-center justify-center space-x-2 ">
-          <p className='cursor-pointer' onClick={()=>deleteComment(c._id)}>
-            <MdDelete />
-          </p>
-        </div>:""
-
-            }
-        
-      </div> 
+        <p>{new Date(c.updatedAt).toString().slice(0, 15)}</p>
+        <p>{new Date(c.updatedAt).toString().slice(16, 24)}</p>
+        {user?._id === c?.userId && (
+          <div className="flex items-center justify-center space-x-2">
+            <p
+              className="cursor-pointer"
+              onClick={() => deleteComment(c?._id)}
+              disabled={isDeleting}
+            >
+              <MdDelete />
+            </p>
+          </div>
+        )}
+      </div>
     </div>
     <p className="px-4 mt-2">{c.comment}</p>
   </div>
-  )
-}
+);
+};
 
 export default Comment
